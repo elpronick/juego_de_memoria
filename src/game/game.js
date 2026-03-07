ffbd1ab49e91858be33d85d8f2cea70a584dbb7f
Francisco Gerardo Tillhet Avomo <elpronick.dev@gmail.com>
2026-03-07 07:14:23 +0100

    Vite creado y todas las hojas vinculadas

---
+++ src/game/game.js
@@ -0,0 +1,9 @@
+/* Este archivo será el cerebro del juego.Se encargará de:
+- crear el mazo de cartas
+- mezclar las cartas
+- guardar el estado del juego
+- iniciar la partida
+Más adelante también manejará:
+- comprobación de parejas
+- movimientos
+- victoria */
